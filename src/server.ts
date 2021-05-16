5d9189e4297ce67ec16ab3f5fe40fefd8b8d5828
Thomas Soares <thomas__soares@hotmail.com>
2021-05-15 22:30:54 -0300

    Indentaçao e metodo post.

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,13 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({
-        message: "Olá NLW 05!!!",
-    });
+  return response.json({
+    message: "Olá NLW 05!!!",
+  });
+});
+
+app.post('/', (request, response) => {
+  return response.json({
+    message: "Usuario salvo com sucesso!!!",
+  });
 });
