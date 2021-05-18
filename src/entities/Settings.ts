7e2b1a4061af5c72a5afd42189c1aee87d1984c9
Thomas Soares <thomas__soares@hotmail.com>
2021-05-17 23:12:15 -0300

    Utilizando os notations.

--- src/entities/Settings.ts
+++ src/entities/Settings.ts
@@ -1,10 +1,16 @@
 import { Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryColumn } from 'typeorm';
 
+@Entity("settings")
 class Setting {
+  @PrimaryColumn()
   id: string;
+  @Column()
   username: string;
+  @Column()
   chat: boolean;
+  @UpdateDateColumn()
   updated_at: Date;
+  @CreateDateColumn()
   created_at: Date;
 }
 
