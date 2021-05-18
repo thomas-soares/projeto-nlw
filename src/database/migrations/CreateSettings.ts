297425fb410f37d2477eeb44d02441cd35901574
Thomas Soares <thomas__soares@hotmail.com>
2021-05-17 22:50:12 -0300

    Criando tabela de settings.

--- src/database/migrations/CreateSettings.ts
+++ src/database/migrations/CreateSettings.ts
@@ -1,9 +1,20 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateSettings implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        
+        await queryRunner.createTable(
+          new Table({
+            name: "settings",
+            columns: [
+              {
+                name: "id",
+                type: "uuid",
+                isPrimary: true
+              }
+            ]
+          })
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
