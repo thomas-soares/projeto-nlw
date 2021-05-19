a4c87dbd58c9d340b1db0e97e080422431a256ee
Thomas Soares <thomas__soares@hotmail.com>
2021-05-18 22:59:50 -0300

    Criando rota de settings e salvando os dados de retorno.

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,20 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+
+import { SettingsRepository } from './repositories/SettingsRepository';
 
 const routes = Router();
+
+routes.post("/settings", async (request, response) => {
+  const settingsRepository = getCustomRepository(SettingsRepository);
+
+  const settings = settingsRepository.create({
+    chat,
+    username
+  });
+
+  await settingsRepository.save(settings);
+  
+});
 
 export { routes };
