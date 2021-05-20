1fb1b35125b97b6e8e0083b2aaf845f7056b3e4f
Thomas Soares <thomas__soares@hotmail.com>
2021-05-19 22:54:04 -0300

    Importando e utilizando as rotas.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 
 import './database';
+import { routes } from './routes';
 
 const app = express();
+
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
