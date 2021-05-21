3096e027e464809c231a839d29af724106e45981
Thomas Soares <thomas__soares@hotmail.com>
2021-05-20 23:13:54 -0300

    Mudando a responsabilidade da controller para a service.

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -7,15 +7,6 @@ class SettingsController {
   async create(request: Request, response: Response) {
     const { chat, username } = request.body;
 
-    const settingsRepository = getCustomRepository(SettingsRepository);
-  
-    const settings = settingsRepository.create({
-      chat,
-      username
-    });
-  
-    await settingsRepository.save(settings);
-  
     return response.json(settings);
   };
 };
