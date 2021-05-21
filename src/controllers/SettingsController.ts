2a451348779f546e7076f8fd7fdbf0255b5afe33
Thomas Soares <thomas__soares@hotmail.com>
2021-05-20 23:23:11 -0300

    Utilizando a service.

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from 'express';
-import { getCustomRepository } from 'typeorm';
-import { SettingsRepository } from '../repositories/SettingsRepository';
+import { SettingsService } from '../services/SettingsService';
 
 class SettingsController {
 
   async create(request: Request, response: Response) {
     const { chat, username } = request.body;
+
+    const settingsService = new SettingsService();
+
+    const settings = await settingsService.create({ chat, username });
 
     return response.json(settings);
   };
