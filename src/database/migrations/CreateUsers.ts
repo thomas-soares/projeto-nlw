6a69f9858bc9ba245a6794833105fe65b1306120
Thomas Soares <thomas__soares@hotmail.com>
2021-05-21 22:43:03 -0300

    Migration de usuarios.

---
+++ src/database/migrations/CreateUsers.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+      
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    
+  }
+    
+}
