95e52dc21348e0d03f16db7cf6aab9e36e0aefdc
Thomas Soares <thomas__soares@hotmail.com>
2021-05-22 22:13:53 -0300

    Metodo de criar usuario.

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,8 +1,27 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 
 class UsersService {
-  
-  
+  async create(email: string) {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const userExists = await usersRepository.findOne({
+      email
+    })
+
+    if (userExists) {
+      return userExists;
+    }
+
+    const user = userRepository.create({
+      email
+    })
+
+    await usersRepository.save(user);
+
+    return user;
+  };
 };
 
 export { UsersService };
