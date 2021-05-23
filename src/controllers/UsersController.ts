5141748f4e924a2624333d3e8373b3032366cc97
Thomas Soares <thomas__soares@hotmail.com>
2021-05-22 22:22:41 -0300

    Retorno da controller.

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -4,10 +4,11 @@ import { UsersService } from "../services/UsersService";
 class UsersController {
   async create(request: Request, response: Response): Promise<Response> {
     const { email } = request.body;
-
     const usersService = new UsersService();
-
     const user = await usersService.create(email);
+
+    return response.json(user);
+
   }
 };
 
