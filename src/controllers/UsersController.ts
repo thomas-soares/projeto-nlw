c2068191ad4cd0f950ef5e0dcab7dc0b475a2e3a
Thomas Soares <thomas__soares@hotmail.com>
2021-05-22 22:17:39 -0300

    Metodo de criar usario na controller.

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,7 +1,14 @@
-
+import { Request, Response } from "express";
+import { UsersService } from "../services/UsersService";
 
 class UsersController {
+  async create(request: Request, response: Response) {
+    const { email } = request.body;
 
+    const usersService = new UsersService();
+
+    await usersService.create();
+  }
 };
 
 export { UsersController };
