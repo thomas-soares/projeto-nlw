5343924a14338273eb7d9513617081383b667f93
Thomas Soares <thomas__soares@hotmail.com>
2021-05-23 23:12:10 -0300

    Migration de messages.

---
+++ src/database/migrations/CreateMessages.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateMessages implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+      
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    
+  }
+    
+}
