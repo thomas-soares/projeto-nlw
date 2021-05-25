d085356b0aafec89f4c9835dbbd11c139cf2bd01
Thomas Soares <thomas__soares@hotmail.com>
2021-05-24 22:48:14 -0300

    Relacionamento da tabela de messages com a de users.

--- src/entities/Message.ts
+++ src/entities/Message.ts
@@ -1,5 +1,6 @@
-import { Entity, PrimaryColumn, CreateDateColumn, Column } from 'typeorm';
+import { Entity, PrimaryColumn, CreateDateColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { v4 as uuid } from 'uuid';
+import { User } from './User';
 
 @Entity("messages")
 class Message {
@@ -12,7 +13,12 @@ class Message {
   
   @Column()
   text: string;
+
+  @JoinColumn({ name: "user_id" })
+  @ManyToOne(() => User)
+  user: User
   
+  @Column()
   user_id: string;
   
   @CreateDateColumn()
