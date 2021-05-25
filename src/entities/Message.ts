f3057e43c60c4f484dad2e938a183fb14c2822a7
Thomas Soares <thomas__soares@hotmail.com>
2021-05-24 22:40:24 -0300

    Mapeando a entidade.

--- src/entities/Message.ts
+++ src/entities/Message.ts
@@ -1,6 +1,23 @@
+import { Entity, PrimaryColumn, CreateDateColumn, Column } from 'typeorm';
 
-
+@Entity("messages")
 class Message {
+  
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  admin_id: string;
+  
+  @Column()
+  text: string;
+  
+  user_id: string;
+  
+  @CreateDateColumn()
+  created_at: Date;
+
+  
 
 };
 
