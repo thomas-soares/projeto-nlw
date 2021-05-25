3a9d10146178bd24d9de65e1c9c818a1d304b5c1
Thomas Soares <thomas__soares@hotmail.com>
2021-05-24 22:57:37 -0300

    Metodo para criar message.

--- src/services/MessagesService.ts
+++ src/services/MessagesService.ts
@@ -1,7 +1,18 @@
+import { getCustomRepository } from "typeorm";
+import { MessagesRepository } from "../repositories/MessagesRepository";
 
 
 class MessagesService {
 
+  async create() {
+    const messagesRepository = getCustomRepository(MessagesRepository);
+
+    messagesRepository.create({
+      admin_id,
+      text,
+      user_id
+    })
+  }
 };
 
 export { MessagesService };
