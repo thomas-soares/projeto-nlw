bcc481ccc323d2b5b54e76662c60a7d2a59ea664
Thomas Soares <thomas__soares@hotmail.com>
2021-05-25 22:44:40 -0300

    Metodo de criar mensagem na controller.

--- src/controllers/MessagesController.ts
+++ src/controllers/MessagesController.ts
@@ -1,8 +1,17 @@
-
+import { Request, Response } from 'express';
+import { MessagesService } from '../services/MessagesService';
 
 class MessagesController {
 
-  async create();
+  async create(request: Request, response: Response) {
+    const messagesService = new MessagesService();
+
+    const message = await messagesService.create({
+      admin_id,
+      text,
+      user_id
+    });
+  };
 };
 
 export { MessagesController };
