94659c14d476eb049f4fcab7cadec0bba83efc8e
Thomas Soares <thomas__soares@hotmail.com>
2021-05-25 22:53:05 -0300

    Recebendo dados e retonando json.

--- src/controllers/MessagesController.ts
+++ src/controllers/MessagesController.ts
@@ -4,6 +4,7 @@ import { MessagesService } from '../services/MessagesService';
 class MessagesController {
 
   async create(request: Request, response: Response) {
+    const { admin_id, text, user_id } = request.body;
     const messagesService = new MessagesService();
 
     const message = await messagesService.create({
@@ -11,6 +12,8 @@ class MessagesController {
       text,
       user_id
     });
+
+    return response.json(message);
   };
 };
 
