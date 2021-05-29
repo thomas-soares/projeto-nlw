2b279508c8af949da9ba224b956b8538a2a43a96
Thomas Soares <thomas__soares@hotmail.com>
2021-05-29 17:56:24 -0300

    Rota para testar a view de client.

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,10 @@ app.set("views", path.join(__dirname, "..", "public"));
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
+app.get("/pages/client", (request, response) => {
+  return response.render("html/client.html");
+});
+
 const http = createServer(app);
 const io = new Server(http);
 
