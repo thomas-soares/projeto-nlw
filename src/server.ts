cf9338661f1087334cfbc6bcde6a38d4c4bc67dd
Thomas Soares <thomas__soares@hotmail.com>
2021-05-28 23:03:03 -0300

    Criando protocolo de http.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { createServer } from 'http';
 
 import './database';
 import { routes } from './routes';
 
 const app = express();
+
+const http = createServer(app);
 
 app.use(express.json());
 
