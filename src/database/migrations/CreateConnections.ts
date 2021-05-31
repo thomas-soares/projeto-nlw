8f01901d7683f40654511257266936f48a886fe3
Thomas Soares <thomas__soares@hotmail.com>
2021-05-30 23:31:31 -0300

    Inicio do metodo pra criar a tabela

--- src/database/migrations/CreateConnections.ts
+++ src/database/migrations/CreateConnections.ts
@@ -1,9 +1,17 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateConnections implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    
+    await queryRunner.createTable(
+        new Table({
+          name: "connections",
+          columns: [
+            
+          ],
+          
+        })
+    )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
