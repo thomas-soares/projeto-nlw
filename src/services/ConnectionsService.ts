08ccddf3281e99cb97c61ad27d4beae4a45ece8f
Thomas Soares <thomas__soares@hotmail.com>
2021-05-31 23:30:41 -0300

    Criaçao da interface e passando parametros que a funçao create vai receber.

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -1,8 +1,15 @@
 import { getCustomRepository } from "typeorm";
 import { ConnectionsRepository } from "../repositories/ConnectionsRepository";
 
+interface IConnectionCreate {
+  socket_id: string;
+  user_id: string;
+  admin_id?: string;
+  id?: string;
+}
+
 class ConnectionsService {
-  async create() {
+  async create({ socket_id, user_id, admin_id, id }: IConnectionCreate) {
     
   }
 };
