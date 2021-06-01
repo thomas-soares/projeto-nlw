4b614d8846277ab73b895fae45c48a15d8c54dcb
Thomas Soares <thomas__soares@hotmail.com>
2021-05-31 23:38:46 -0300

    Importando connectionService e utilizando pra criar conexao.

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,7 +1,13 @@
 import { io } from '../http';
+import { ConnectionsService } from '../services/ConnectionsService';
 
 io.on("connect", (socket) => {
-  socket.on("client_first_access", (params) => {
+  socket.on("client_first_access", async (params) => {
     console.log(params);
+
+    await connectionsService.create({
+      socket_id,
+      user_id
+    });
   });
 });
