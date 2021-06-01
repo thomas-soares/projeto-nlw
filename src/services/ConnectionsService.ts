ad831214c7f2e89bf9e024a5e05991d0e4297299
Thomas Soares <thomas__soares@hotmail.com>
2021-05-31 23:27:18 -0300

    Service de connections.

---
+++ src/services/ConnectionsService.ts
@@ -0,0 +1,10 @@
+import { getCustomRepository } from "typeorm";
+import { ConnectionsRepository } from "../repositories/ConnectionsRepository";
+
+class ConnectionsService {
+  async create() {
+    
+  }
+};
+
+export { ConnectionsService };
