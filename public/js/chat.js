da4db01cef6b754484fdfa1418659e8e5f1882fe
Thomas Soares <thomas__soares@hotmail.com>
2021-06-03 23:20:38 -0300

    Listando as mensagens.

--- public/js/chat.js
+++ public/js/chat.js
@@ -26,6 +26,17 @@ document.querySelector("#start_chat").addEventListener("click", (event) => {
   });
 
   socket.on("client_list_all_messages", (messages) => {
-    console.log("messages", messages);
+    var template_client = document.getElementById("message-user-template").innerHTML;
+    var template_admin = document.getElementById("admin-template").innerHTML;
+
+    messages.forEach(message => {
+      if (message.admin_id === null) {
+        const rendered = Mustache.render(template_client, {
+          message: message.text,
+          email
+        });
+
+      }
+    });
   });
 });
