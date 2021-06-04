d9b2f9f919edade22061fa6e10134be603c13a2d
Thomas Soares <thomas__soares@hotmail.com>
2021-06-03 23:20:59 -0300

    Colocando mensagens na view.

--- public/js/chat.js
+++ public/js/chat.js
@@ -36,6 +36,7 @@ document.querySelector("#start_chat").addEventListener("click", (event) => {
           email
         });
 
+        document.getElementById("messages").innerHTML += rendered;
       }
     });
   });
