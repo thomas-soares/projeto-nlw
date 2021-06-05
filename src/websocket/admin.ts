71edbc770da9b315c18912987d9207af0745c258
Thomas Soares <thomas__soares@hotmail.com>
2021-06-04 23:29:55 -0300

    Emitindo evento para trazer as conversas.

--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -4,4 +4,6 @@ import { ConnectionsService } from "../services/ConnectionsService";
 io.on("connect", async (socket) => {
   const connectionsService = new ConnectionsService();
   const allConnectionsWithoutAdmin = await connectionsService.findAllWithoutAdmin();
+
+  io.emit("admin_list_all_users", allConnectionsWithoutAdmin);
 });
