6405797575a0d09eb22935da29065471fae336a3
Thomas Soares <thomas__soares@hotmail.com>
2021-06-04 23:15:15 -0300

    Instanciando service.

--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -1,2 +1,6 @@
 import { io } from "../http";
+import { ConnectionsService } from "../services/ConnectionsService";
 
+io.on("connect", (socket) => {
+  const connectionsService = new ConnectionsService();
+});
