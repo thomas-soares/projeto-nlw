aa67a2bce4ad2a16386d56051c0febcd30b599d2
Thomas Soares <thomas__soares@hotmail.com>
2021-06-06 23:43:41 -0300

    monitorando evento.

--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -6,4 +6,8 @@ io.on("connect", async (socket) => {
   const allConnectionsWithoutAdmin = await connectionsService.findAllWithoutAdmin();
 
   io.emit("admin_list_all_users", allConnectionsWithoutAdmin);
+
+  socket.on("admin_list_messages_by_user", params => {
+
+  });
 });
