ddd35db963d762280af82712b18debd0f8ee7f33
Thomas Soares <thomas__soares@hotmail.com>
2021-06-07 23:19:50 -0300

    Corrigindo atribuiçao e colocando em mais de um elemento.

--- public/js/admin.js
+++ public/js/admin.js
@@ -42,13 +42,14 @@ function call(id) {
       if (message.admin_id === null) {
         createDiv.className = "admin_message_client";
 
-        createDiv.innerHTML = `<span>${connection.user.email} - ${message.text}</span>`;
-        createDiv.innerHTML = `<span class="admin_date">${dayjs(message.created_at).format("DD/MM/YYYY HH:mm:ss")}</span>`;
+        createDiv.innerHTML = `<span>${connection.user.email}</span>`;
+        createDiv.innerHTML += `<span>${message.text}</span>`;
+        createDiv.innerHTML += `<span class="admin_date">${dayjs(message.created_at).format("DD/MM/YYYY HH:mm:ss")}</span>`;
       } else {
         createDiv.className = "admin_message_admin";
 
         createDiv.innerHTML = `Atendente: <span>${message.text}</span>`;
-        createDiv.innerHTML = `<span class="admin_date">${dayjs(message.created_at).format("DD/MM/YYYY HH:mm:ss")}</span>`;
+        createDiv.innerHTML += `<span class="admin_date">${dayjs(message.created_at).format("DD/MM/YYYY HH:mm:ss")}</span>`;
       }
 
       divMessages.appendChild(createDiv);
