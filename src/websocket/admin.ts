86ab41681da4e61e860f3f5dff182a7c6556cd1a
Thomas Soares <thomas__soares@hotmail.com>
2021-06-08 23:00:35 -0300

    Passando text e socket id pro evento.

--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -27,6 +27,9 @@ io.on("connect", async (socket) => {
 
     const { socket_id } = await connectionsService.findByUserId(user_id);
 
-    io.to(socket_id).emit("admin_send_to_client")
+    io.to(socket_id).emit("admin_send_to_client", {
+      text,
+      socket_id: socket.id
+    });
   });
 });
