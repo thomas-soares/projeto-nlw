6676b2655730229364e97606103676562ed0429c
Thomas Soares <thomas__soares@hotmail.com>
2021-06-10 22:44:10 -0300

    Params do evento.

--- public/js/chat.js
+++ public/js/chat.js
@@ -58,5 +58,10 @@ document.querySelector("#start_chat").addEventListener("click", (event) => {
 document.querySelector("#send_message_button").addEventListener("click", (event) => {
   const text = document.getElementById("message_user");
 
+  const params ={
+    text,
+    socket_admin_id
+  }
+
   socket.emit("client_send_to_admin", params);
 });
