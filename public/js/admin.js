3116883618fdbdaefd41b7ae885c4cd37d2f7623
Thomas Soares <thomas__soares@hotmail.com>
2021-06-12 22:43:13 -0300

    Pegando os dados do params.

--- public/js/admin.js
+++ public/js/admin.js
@@ -84,6 +84,6 @@ socket.on("admin_receive_message", (data) => {
   createDiv.className = "admin_message_client";
 
   createDiv.innerHTML = `<span>${connection.user.email}</span>`;
-  createDiv.innerHTML += `<span>${message.text}</span>`;
-  createDiv.innerHTML += `<span class="admin_date">${dayjs(message.created_at).format("DD/MM/YYYY HH:mm:ss")}</span>`;
+  createDiv.innerHTML += `<span>${params.message.text}</span>`;
+  createDiv.innerHTML += `<span class="admin_date">${dayjs(params.message.created_at).format("DD/MM/YYYY HH:mm:ss")}</span>`;
 });
